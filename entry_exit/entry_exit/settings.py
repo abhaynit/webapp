849f2cbbef4faab9ae41fde151b3d0fed1c676e6
abhaynit <87191075+abhaynit@users.noreply.github.com>
2022-01-30 08:11:58 +0530

    Commit

--- entry_exit/entry_exit/settings.py
+++ entry_exit/entry_exit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q7jvwk^mb=cc=gft$6poijikxzo^*l*(!=ebtgdps99ppgnxne
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','nitnagaland.herokuapp.com']
 
 
 # Application definition
