bbb3d00f02097d16002f7e8460c8760da0a67157
abhaynit <87191075+abhaynit@users.noreply.github.com>
2022-01-30 10:21:17 +0530

    commit

--- entry_exit/settings.py
+++ entry_exit/settings.py
@@ -119,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+import os
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
 # Default primary key field type
